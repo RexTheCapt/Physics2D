15fd2452d8044ebdbc5de1079d4f630e9723ddbd
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-05 15:17:12 +0100

    Created script

---
+++ Assets/Scripts/PlatformRotationControl.cs
@@ -0,0 +1,21 @@
+﻿#region Usings
+
+using JetBrains.Annotations;
+using UnityEngine;
+
+#endregion
+
+namespace Assets.Scripts
+{
+    public class PlatformRotationControl : MonoBehaviour
+    {
+        // ReSharper disable once MemberCanBePrivate.Global
+        public Transform TargetTransform;
+
+        [UsedImplicitly]
+        void Update()
+        {
+            transform.LookAt(TargetTransform);
+        }
+    }
+}
