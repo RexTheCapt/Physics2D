5fbf7b59290f0181e95f6dc35d4de89a379c6d80
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-05 10:04:21 +0100

    Mouse Cursor Interaction

--- Assets/Scripts/CursorControl.cs
+++ Assets/Scripts/CursorControl.cs
@@ -1,5 +1,6 @@
 ﻿#region Usings
 
+using JetBrains.Annotations;
 using UnityEngine;
 
 #endregion
@@ -8,9 +9,20 @@ namespace Assets.Scripts
 {
     public class CursorControl : MonoBehaviour
     {
+        [UsedImplicitly]
         private void Update()
         {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(hit.transform.gameObject.name);
+                    Destroy(hit.transform.gameObject);
+                }
+            }
         }
     }
 }
