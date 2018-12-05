2ca4b8e846547de719b2d57ff2455f56d967c807
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-05 12:15:39 +0100

    Fixed ReSharper complaints

--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -10,16 +10,17 @@ namespace Assets.Scripts
     public class MouseDrag : MonoBehaviour
     {
         public float DropVelocity = 5;                                              // When object is no longer being dragged this force is applied.
-        public bool isDraggable = false;                                            // Toggle if object can be dragged or not.
+        public bool IsDraggable = false;                                            // Toggle if object can be dragged or not.
 
         private Vector3 _screenPoint;                                               // Stores transformed position from world space into screen space.
         private Vector3 _offset;                                                    // Offset for dragging the object.
         private Vector3[] positionVector3 = new Vector3[2];                         // Store current and last position of object.
-        private bool DisableDrag;                                                   // A boolean to disable dragging if something happens.
+        private bool _disableDrag;                                                  // A boolean to disable dragging if something happens.
 
         /// <summary>
         /// Runs every frame.
         /// </summary>
+        [UsedImplicitly]                                                            // Tell ReSharper this function is in use.
         void Update()
         {
             positionVector3[1] = positionVector3[0];                                // Change current position to last position.
@@ -32,12 +33,12 @@ namespace Assets.Scripts
         [UsedImplicitly]                                                        // Tell ReSharper this function is in use.
         void OnMouseDown()
         {
-            if (isDraggable)                                                        // If object is draggable then run this block.
+            if (IsDraggable)                                                        // If object is draggable then run this block.
             {
                 _screenPoint = Camera.main.WorldToScreenPoint(transform.position);  // Store where player clicked on the screen.
                                                                                     // Create an offset relative to the camera and object.
                 _offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
-                DisableDrag = false;                                                // Make sure value is true.
+                _disableDrag = false;                                                // Make sure value is true.
             }
         }
 
@@ -47,7 +48,7 @@ namespace Assets.Scripts
         [UsedImplicitly]                                                        // Tell ReSharper this function is in use.
         void OnMouseDrag()
         {
-            if (!DisableDrag && isDraggable)                                        // Check if object can be dragged.
+            if (!_disableDrag && IsDraggable)                                        // Check if object can be dragged.
             {
                                                                                     // Store current mouse position.
                 Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
@@ -68,11 +69,12 @@ namespace Assets.Scripts
         /// Called when object is colliding with an another object.
         /// </summary>
         /// <param name="collision"></param>
+        [UsedImplicitly]                                                             // Tell ReSharper this function is in use.
         void OnCollisionEnter(Collision collision)
         {
-            if (!DisableDrag)                                                       // Check if value is false.
-                transform.position = positionVector3[1];                            // Transform object to last position.
-            DisableDrag = true;                                                     // Disable dragging.
+            if (!_disableDrag)                                                       // Check if value is false.
+                transform.position = positionVector3[1];                             // Transform object to last position.
+            _disableDrag = true;                                                     // Disable dragging.
         }
     }
 }
