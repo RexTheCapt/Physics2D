956d8844cf8ff3127606c3b819c3b198a463cdfd
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-06 14:35:34 +0100

    Created camera pan function

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,27 @@
+﻿#region Usings
+
+using JetBrains.Annotations;
+using UnityEngine;
+
+#endregion
+
+namespace Assets.Scripts
+{
+    public class CameraPan : MonoBehaviour
+    {
+        private Vector3 _lastPosition;
+
+        [UsedImplicitly]
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire2")) _lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) ;
+
+            if (Input.GetButton("Fire2"))
+            {
+                Vector3 delta = Camera.main.ScreenToWorldPoint(Input.mousePosition)  - _lastPosition;
+                transform.Translate(0 - delta.x, 0 - delta.y, 0);
+                _lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) ;
+            }
+        }
+    }
+}
