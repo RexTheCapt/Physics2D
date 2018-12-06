22d6de9c42ff09fdd99c5f1ff7e76b23a78a8282
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-06 15:32:37 +0100

    Created exit script

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,17 @@
+﻿#region Usings
+
+using UnityEngine;
+
+#endregion
+
+public class ExitGame : MonoBehaviour
+{
+    public float exitTime = 5f;
+    private float timer;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
