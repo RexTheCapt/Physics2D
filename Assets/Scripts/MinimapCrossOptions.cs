a179be48a40f9e599718c2ee1dbf65fa3792a90a
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-07 10:48:21 +0100

    Created minimap

---
+++ Assets/Scripts/MinimapCrossOptions.cs
@@ -0,0 +1,25 @@
+﻿#region Usings
+
+using UnityEngine;
+
+#endregion
+
+public class MinimapCrossOptions : MonoBehaviour
+{
+    [Range(0f, 1f)] public float alpha = 1f;
+    public GameObject[] SquareGameObjects;
+
+    void Update()
+    {
+        foreach (GameObject squareGameObject in SquareGameObjects)
+        {
+            Renderer renderer = squareGameObject.GetComponent<Renderer>();
+            Material material = renderer.material;
+            Color color = material.color;
+
+            color.a = alpha;
+
+            material.color = color;
+        }
+    }
+}
