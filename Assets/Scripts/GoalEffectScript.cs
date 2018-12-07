c8427c065c7e7ede90446c1ef63f5be94e7aaa37
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-07 13:19:59 +0100

    update

---
+++ Assets/Scripts/GoalEffectScript.cs
@@ -0,0 +1,18 @@
+﻿#region Usings
+
+using UnityEngine;
+
+#endregion
+
+public class GoalEffectScript : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,26 @@
+﻿#region Usings
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#endregion
+
+namespace Assets.Scripts
+{
+    public class GoalScript : MonoBehaviour
+    {
+        public int requiredWins = 1;
+        public int currentWins = 0;
+
+        void OnTriggerEnter(Collider collision)
+        {
+            string tag = collision.gameObject.tag;
+
+            if (tag == "Ball")
+            {
+                currentWins++;
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
