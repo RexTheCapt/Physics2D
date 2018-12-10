b46b90d0338329026b317b48e129bf899433be6b
RexTheCapt <cato.jordalen@hotmail.com>
2018-12-10 14:44:16 +0100

    Update GoalEffectScript.cs

--- Assets/Scripts/GoalEffectScript.cs
+++ Assets/Scripts/GoalEffectScript.cs
@@ -1,18 +1,54 @@
 ﻿#region Usings
 
+using JetBrains.Annotations;
 using UnityEngine;
 
 #endregion
 
-public class GoalEffectScript : MonoBehaviour
+namespace Assets.Scripts
 {
-    // Use this for initialization
-    private void Start()
+    public class GoalEffectScript : MonoBehaviour
     {
-    }
+        [SerializeField]
+        private float _deltaTime;
+        public float MaxScale = 2f;
+        public float MinScale = 1f;
+        public float Speed = 1f;
+        public int Stage;
 
-    // Update is called once per frame
-    private void Update()
-    {
+        // Update is called once per frame
+        [UsedImplicitly]
+        private void Update()
+        {
+            _deltaTime = Time.deltaTime;
+
+            Vector3 scale = gameObject.transform.localScale;
+
+            switch (Stage)
+            {
+                case 0: // Expand
+                    scale += GetAddition();
+
+                    if (scale.y >= MaxScale)
+                        Stage++;
+                    break;
+                case 1: // Implode
+                    scale -= GetAddition();
+
+                    if (scale.y <= MinScale)
+                        Stage++;
+                    break;
+                case 2: // Destroy
+                    Destroy(gameObject);
+                    break;
+            }
+
+            transform.localScale = scale;
+        }
+
+        private Vector3 GetAddition()
+        {
+            return new Vector3(Speed * _deltaTime, Speed * _deltaTime, Speed * _deltaTime);
+        }
     }
 }
