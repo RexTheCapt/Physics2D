6f20ec8ce07e42ba5b0f410a9b73affefdc2ead5
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2018-12-12 16:00:35 +0100

    Create EnableCanvas.cs

---
+++ Assets/Scripts/Single Functions/EnableCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCanvas : MonoBehaviour
+{
+    public GameObject CanvasGameObject;
+
+    void Start()
+    {
+        CanvasGameObject.SetActive(true);
+    }
+}
