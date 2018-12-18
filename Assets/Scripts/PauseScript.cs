e0543f3fb907276f34203b0c5224210ca4d10f19
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2018-12-18 12:34:27 +0100

    Create PauseScript.cs

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,24 @@
+﻿#region usings
+
+using UnityEngine;
+
+#endregion
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject[] PlayGameObjects;
+    public GameObject[] PauseGameObjects;
+    public bool GamePaused
+    {
+        get { return pause; }
+    }
+
+    private bool pause;
+
+    public void Toggle()
+    {
+        pause = !pause;
+
+
+    }
+}
