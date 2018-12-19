f41d6fe33dcae11d82a47bd2f3702c2ac1f00ee7
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2018-12-19 10:37:50 +0100

    Disabled pan when in pause menu

--- Assets/Scripts/Single Functions/CameraPan.cs
+++ Assets/Scripts/Single Functions/CameraPan.cs
@@ -10,17 +10,21 @@ namespace Assets.Scripts.Single_Functions
     public class CameraPan : MonoBehaviour
     {
         private Vector3 _lastPosition;
+        public PauseScript PauseScript;
 
         [UsedImplicitly]
         private void Update()
         {
-            if (Input.GetButtonDown("Fire2")) _lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) ;
-
-            if (Input.GetButton("Fire2"))
+            if (!PauseScript.GamePaused)
             {
-                Vector3 delta = Camera.main.ScreenToWorldPoint(Input.mousePosition)  - _lastPosition;
-                transform.Translate(0 - delta.x, 0 - delta.y, 0);
-                _lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) ;
+                if (Input.GetButtonDown("Fire2")) _lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                if (Input.GetButton("Fire2"))
+                {
+                    Vector3 delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - _lastPosition;
+                    transform.Translate(0 - delta.x, 0 - delta.y, 0);
+                    _lastPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                }
             }
         }
     }
